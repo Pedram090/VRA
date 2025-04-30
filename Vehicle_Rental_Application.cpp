3b048f9beffc9e58359610dfe8d57d235036e5b1
Pedram <mohammadipedram090@gmail.com>
2025-04-30 09:46:02 +0330

    changing name

---
+++ Vehicle_Rental_Application.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class User {
+private:
+    string username;
+    string password;
+public:
+   User(const string& user, const string& pass)
+        : username(user), password(pass) {}
+    void logIn() {}
+    void logOut(){}
+};
+
+class App {
+private:
+    vector<User> Users;
+public:
+    void newUser(string username, string pass){
+        Users.push_back(User(username, pass));
+    }
+
+
+};
+//inital
+
+int main(){
+
+
+
+
+
+return 0;
+}
